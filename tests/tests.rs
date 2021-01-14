cd063e957d18a239f926365aa4e5cd4ff1067e7e
Graeme Read <graeme@sigma957.net>
2021-01-14 17:07:54 +0000

    Add tests to check the length of code_verifier
    NOTE: code_verifier_valid_length is currently failing

--- tests/tests.rs
+++ tests/tests.rs
@@ -19,3 +19,20 @@ fn verify_pregenerated_challenges() {
     code_challenge = pkce::code_challenge(b"Ma la volpe col suo balzo ha raggiunto il quieto Fido");
     assert_eq!(code_challenge, "Wqg2eog5_VX3Opr14PYaKUImbpHzkK1aFLZvkYNbQkM");
 }
+
+
+#[test]
+fn code_verifier_valid_length() {
+    // See https://tools.ietf.org/html/rfc7636#section-4.1
+    assert_eq!(43, pkce::code_verifier(43).len());
+    assert_eq!(128, pkce::code_verifier(128).len());
+}
+
+
+#[test]
+#[should_panic]
+fn code_verifier_invalid_length() {
+    // See https://tools.ietf.org/html/rfc7636#section-4.1
+    pkce::code_verifier(42);
+    pkce::code_verifier(128);
+}
