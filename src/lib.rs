38a9aff564d1d5fd4f9e58f8418f884017b1bf53
Graeme Read <graeme@sigma957.net>
2021-01-15 09:51:52 +0000

    Implement suggested changes by clippy
    Suggestions from `cargo clippy`. See https://github.com/rust-lang/rust-clippy.

--- src/lib.rs
+++ src/lib.rs
@@ -28,27 +28,26 @@ const CHARS: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
 ///
 /// * `length` - The desired length in bytes of the code verifier. This value should be between 43 and 128 or else the function will panic.
 pub fn code_verifier(length: usize) -> Vec<u8> {
-    assert!(length >= 43 && length <= 128, "Code verifier length must be between 43 and 128 bytes");
+    assert!((43..=128).contains(&length), "Code verifier length must be between 43 and 128 bytes");
 
     let mut rng = thread_rng();
 
-    return (0..length).map(|_| {
+    (0..length).map(|_| {
         let i = rng.gen_range(0, CHARS.len());
         CHARS[i]
-    })
-    .collect();
+    }).collect()
 }
 
 fn base64_url_encode(input: &[u8]) -> String {
     let b64 = base64::encode(input);
-    return b64.chars().filter_map(|c| {
+    b64.chars().filter_map(|c| {
         match c {
             '=' => None,
             '+' => Some('-'),
             '/' => Some('_'),
             x => Some(x)
         }
-    }).collect();
+    }).collect()
 }
 
 /// Generate a code challenge from a given code verifier with SHA256 and base64. 
@@ -60,5 +59,5 @@ pub fn code_challenge(code_verifier: &[u8]) -> String {
     let mut sha = Sha256::new();
     sha.update(code_verifier);
     let result = sha.finalize();
-    return base64_url_encode(&result[..]);
+    base64_url_encode(&result[..])
 }
